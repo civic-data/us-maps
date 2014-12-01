610ef59bdf04e73f225a783b22acd39235da84ca
Ralph Yozzo <fedex1@gmail.com>
2014-11-30 19:36:45 -0500

    update scripts

--- geojson/runit.sh
+++ geojson/runit.sh
@@ -1,2 +1,3 @@
 #!
-ogr2ogr -f GeoJSON small.json state.json -where 'REGION10="4"'
+# ogr2ogr -f GeoJSON small.json state.json -where 'REGION10="4"'
+ogr2ogr -progress -f GeoJSON nyc-zip.json zcta5.json -where 'GEOID10="11221"'
